ea1a750357383c2b0023ce81ed5bd59c3f325d2a
Anjal Sotang <anjalsotang26@gmail.com>
2025-11-17 10:57:22 +0545

    db connection successfull

--- websocket/server.ts
+++ websocket/server.ts
@@ -1,8 +1,10 @@
 import app from './src/app'
 import {envConfig} from './src/config/config'
+import connectToDB from './src/config/db'
 
 
 const startServer = () => {
+    connectToDB()
     const port = envConfig.port || 4000;
     app.listen(port || 3000, ()=>{
         console.log(`Server started at ${port}`)
