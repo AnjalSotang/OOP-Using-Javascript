6e47dcd1f2d592e4377e88f9837a06bc5f98880b
Anjal Sotang <anjalsotang26@gmail.com>
2025-11-17 10:18:41 +0545

    Server set up sucessfully

---
+++ websocket/server.ts
@@ -0,0 +1,12 @@
+import app from './src/app'
+import {envConfig} from './src/config/config'
+
+
+const startServer = () => {
+    const port = envConfig.port || 4000;
+    app.listen(port || 3000, ()=>{
+        console.log(`Server started at ${port}`)
+    })
+}
+
+startServer()
