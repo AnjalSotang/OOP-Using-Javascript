cf223dbcf3fcabf1101e6b89b83ea79970aa44d4
Anjal Sotang <anjalsotang26@gmail.com>
2025-11-18 16:55:39 +0545

    add Home UI

--- websocket/src/app.ts
+++ websocket/src/app.ts
@@ -1,7 +1,13 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
+import path from 'node:path'
 const app = express()
 import ("../src/todo/todoController")
 
+app.set("views", path.join(__dirname,'views'))
 app.set("view engine", "ejs")
+
+app.get("/", (req:Request,res:Response)=>{
+    res.render("home.ejs")
+})
 
 export default app
